6e78fb08261af428f7727002577aeb616a07a502
Diego Rojas Torrealba <diego_deg_58@hotmail.com>
2024-07-30 23:06:28 -0400

    Agrega archivos para repository

---
+++ app/src/main/java/com/example/ecommerce/model/ProductViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.ecommerce.model
+
+class ProductViewModel {
+}
---
+++ app/src/main/java/com/example/ecommerce/repository/RoomRepositoryImp.kt
@@ -0,0 +1,10 @@
+package com.example.ecommerce.repository
+
+import com.example.ecommerce.data.Product
+
+class RoomRepositoryImp : ProductRepository {
+    override fun getProducts(): List<Product> {
+        TODO("Not yet implemented")
+    }
+
+}
